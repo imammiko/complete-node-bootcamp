20e7fa115c58d3d84b430534be71a51a9d8a5f07
imammiko <sujatmikoi@yahoo.co.id>
2020-04-17 10:18:23 +0700

    13.build a (very)simple api asy

--- 1-node-farm/starter/app.js
+++ 1-node-farm/starter/app.js
@@ -28,6 +28,15 @@ const server = http.createServer((req, res) => {
         res.end('this is the overview');
     } else if (pathName === '/product') {
         res.end('this is the product')
+    } else if (pathName === '/api') {
+        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+            const productData = JSON.parse(data);
+            console.log(productData)
+            res.writeHead(200, {
+                'content-type': 'application/json'
+            });
+            res.end(data);
+        })
     } else {
         res.writeHead(404, {
             'contnt-type': 'text/html',
